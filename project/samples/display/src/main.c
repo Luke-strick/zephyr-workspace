836f82ecb19dc798548b15da1610a7d4c108f78a
Luke <lukearthur2004@gmail.com>
2026-04-07 20:33:06 -0400

    Added RTT to dispaly sample

--- project/samples/display/src/main.c
+++ project/samples/display/src/main.c
@@ -9,8 +9,6 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/uart.h>
-#include <zephyr/usb/usb_device.h>
 #include <zephyr/pm/device_runtime.h>
 #include <display_ui.h>
 
@@ -19,19 +17,6 @@ int main(void)
 	/* Power up the 3V3 and 5V rails */
 	pm_device_runtime_get(DEVICE_DT_GET(DT_NODELABEL(power_3v3)));
 	pm_device_runtime_get(DEVICE_DT_GET(DT_NODELABEL(power_5v)));
-
-	/* Wait for USB CDC terminal */
-	const struct device *usb_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
-
-	if (device_is_ready(usb_dev)) {
-		usb_enable(NULL);
-		uint32_t dtr = 0;
-
-		while (!dtr) {
-			uart_line_ctrl_get(usb_dev, UART_LINE_CTRL_DTR, &dtr);
-			k_sleep(K_MSEC(100));
-		}
-	}
 
 	/* Initialise display */
 	const struct device *disp = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
